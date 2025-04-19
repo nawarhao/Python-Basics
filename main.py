975125430b2fb4617edc8781418e9ee7b058fcd7
nawarhao <nrazonno@gmail.com>
2025-04-19 11:43:48 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# working on print statements
+name = "Razonno"
+print(f"{name} Welcome to Python course")
+print(17+13)
+print(76*4)
+print(5^3)
+
+#Working on multiple print statements
+print("We are learning Python basics")
+print(1,9)
+print("\n We are learning keywords and identifiers \n")
+print("Hello world", 1)
+print("Keywords", end= ".")
+
+#Variables, input methods
+x = 5
+y = "Hello"
+greet = "Good morning"
+name = input("Enter your name")
+print("\n Hi", y, greet, name, "How are you doing today? \n")
+
+#Printing keywords
+import keyword
+print("python keywords \n")
+print(keyword.kwlist)
